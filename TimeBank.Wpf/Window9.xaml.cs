268dc3d90b4cdf795089bbb30fbc1c0860d4b375
AnaIgsan <61803230+AnaIgsan@users.noreply.github.com>
2021-01-02 14:02:22 +0100

    wpf

---
+++ TimeBank.Wpf/Window9.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimeBank.Wpf
+{
+    /// <summary>
+    /// Lógica de interacción para Window9.xaml
+    /// </summary>
+    public partial class Window9 : Window
+    {
+        public Window9()
+        {
+            InitializeComponent();
+        }
+    }
+}
